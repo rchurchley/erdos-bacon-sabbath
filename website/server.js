a8774d62191d393f54dad95b83425d036d8797e7
Ross Churchley <ross@rosschurchley.com>
2017-05-22 16:06:01 -0700

    Reorganize website templates, add search

--- website/server.js
+++ website/server.js
@@ -1,28 +1,29 @@
 // Built-in modules
-var fs = require('fs');
-var path = require('path');
-var request = require('request');
+const fs = require('fs');
+const path = require('path');
+const request = require('request');
 
 // Ensure the working directory is the same as the script directory
 process.chdir(__dirname);
 
-// Populate list of featured people
-var the_list = []
-get_the_list();
+// Populate list of featured people, and randomize the order every hour
+let featured_list = []
+get_featured_list();
+setInterval(function() { shuffle(featured_list); }, 3600000);
 
 /* SETUP *
  * ===== */
 
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 app.set('port', process.env.PORT || 3000);
 
-var expresshandlebars = require('express-handlebars');
+const expresshandlebars = require('express-handlebars');
 app.engine('handlebars', expresshandlebars({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 // Logging
-var morgan = require('morgan');
+const morgan = require('morgan');
 app.use(morgan(':date[clf] | :status :method :url --> :remote-addr (:response-time ms)'));
 
 
@@ -36,17 +37,21 @@ app.use('/api', express.static(path.join(__dirname, '..', 'data'), { dotfiles: '
 app.use('/images', express.static(path.join(__dirname, '..', 'images'), { dotfiles: 'ignore', etag: false, extensions: ['jpeg', 'jpg'] }));
 
 app.get('/', function (req, res) {
-  shuffle(the_list);
-  res.render('the_list', { people: the_list, title: 'Home' });
+  res.render('homepage', { people: featured_list, title: 'Home' });
+});
+
+app.get('/frequently-asked-questions/', function(req, res) {
+  res.render('faq');
 });
 
 app.get('/:person/', function (req, res) {
-  var api_path = path.join(__dirname, '..', 'data', req.params['person'] + '.json');
-  var img_license_path = path.join(__dirname, '..', 'images', req.params['person'], 'LICENSE');
+  let api_path = path.join(__dirname, '..', 'data', req.params['person'] + '.json');
+  let img_license_path = path.join(__dirname, '..', 'images', req.params['person'], 'LICENSE');
   fs.readFile(api_path, 'utf8', function (person_err, person_data) {
     fs.readFile(img_license_path, 'utf8', function (license_err, license_data) {
       if (person_err || license_err) {
-        res.status(404).sendFile('404.html', { root: path.join(__dirname, 'public') });
+        console.log(person_err || license_err);
+        res.status(404).render('404');
       } else {
         person = JSON.parse(person_data);
         license = JSON.parse(license_data);
@@ -75,7 +80,7 @@ app.listen(app.get('port'), function () {
 /* HELPER FUNCTIONS *
  * ================ */
 
-function get_the_list() {
+function get_featured_list() {
   request(
     {
       url: "https://api.github.com/repos/rchurchley/erdos-bacon-sabbath/contents/data",
@@ -84,13 +89,19 @@ function get_the_list() {
     function (error, response, body) {
       if (error) {
         console.error(error);
-        setTimeout(get_the_list, 5000); // retry after five seconds
+        setTimeout(get_featured_list, 5000); // retry after five seconds
       } else {
-        the_list = peopleFromGithubResponse(JSON.parse(body));
-        console.log("Successfully retrieved " + the_list.length + " items from GitHub.");
+        featured_list = peopleFromGithubResponse(JSON.parse(body));
+        console.log("Successfully retrieved " + featured_list.length + " items from GitHub.");
+        shuffle(featured_list);
       }
     }
   );
+  // fs.readdir(path.join(__dirname, '..', 'data'), function (err, items) {
+  //   featured_list = items.filter(function (filename) {
+  //     return filename.endsWith('.json');
+  //   }).map(personObjectFromFilename);
+  // });
 };
 
 function peopleFromGithubResponse(array) {
@@ -119,9 +130,9 @@ function getProperNameFromKebab(kebabCaseName) {
  * See: http://stackoverflow.com/a/12646864
  */
 function shuffle(array) {
-  for (var i = array.length - 1; i > 0; i--) {
-    var j = Math.floor(Math.random() * (i + 1));
-    var temp = array[i];
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    let temp = array[i];
     array[i] = array[j];
     array[j] = temp;
   }
